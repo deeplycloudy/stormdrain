7b1e752a1c031f84a89bbc7b05bcd2b5e4a69052
Eric <eric.bruning@gmail.com>
2013-08-13 21:41:29 -0500

    Named array dataset can now accept updates based on a shared key (index column)

--- data.py
+++ data.py
@@ -1,8 +1,30 @@
+from functools import wraps
+
+import numpy as np
+from numpy.lib.recfunctions import append_fields
+
 from stormdrain.pubsub import get_exchange
+from stormdrain.pipeline import coroutine
 
 class BaseDate(object):
     def __init__(self, date):
         self.date=date
+
+_default_index_name = 'point_id'
+
+def indexed(func, index_name=_default_index_name):
+    """ Decorator to add an index column to the data array in a NamedArrayDataset.
+        Allows filtered arrays be linked to the original array after a lasso
+
+        func returns an instance of NamedArrayDataset.
+    """
+    @wraps(func)
+    def create_indexed(*args, **kwargs):
+        d = func(*args, **kwargs)
+        indices = np.arange(d.data.size)
+        d.data = append_fields(d.data, index_name, indices, usemask=False)
+        return d
+    return create_indexed
 
 
 class NamedArrayDataset(object):
@@ -16,7 +38,26 @@ class NamedArrayDataset(object):
         # dataset objects are not one-shot tasks; they live for the whole lifecycle.
         # Anyway, ignoring for now.
         self.bounds_updated_xchg.attach(self)
+    
+    @coroutine
+    def update(self, index_name=_default_index_name, field_names=None):
+        """ update the values in self.data using data received 
         
+            This function assumes that the shapes of the data are compatible
+            and have enough of the same dtype fields to complete the operation.
+            If field_names is None, the dtypes must match exactly.
+        """
+        while True:
+            a = (yield)
+            indices = a[index_name]
+            if field_names is not None:
+                # update only one field
+                for field_name in field_names:
+                    self.data[field_name][indices] = a[field_name]
+            else:
+                # update everything
+                self.data[indices] = a
+                
         
     def send(self, msg):
         """ SD_bounds_updated messages are sent here """
