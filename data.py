af93e61d47f15d09de0d6594490fe3f5d6c56bbe
Eric <eric.bruning@gmail.com>
2013-10-18 16:15:24 -0500

    Use new data reflow messages instead of a bounds trigger

--- data.py
+++ data.py
@@ -34,13 +34,13 @@ class NamedArrayDataset(object):
     def __init__(self, data, target=None):
         self.target = target
         self.data = data
-        self.bounds_updated_xchg = get_exchange('SD_bounds_updated')
-        
+        self.reflow_start_xchg = get_exchange('SD_reflow_start')
+
         # Need to find a way to detach when "done" with dataset. __del__ doesn't work
         # because of some gc issues. context manager doesn't exactly work, since the
         # dataset objects are not one-shot tasks; they live for the whole lifecycle.
         # Anyway, ignoring for now.
-        self.bounds_updated_xchg.attach(self)
+        self.reflow_start_xchg.attach(self)
     
     @coroutine
     def update(self, index_name=_default_index_name, field_names=None):
@@ -62,7 +62,7 @@ class NamedArrayDataset(object):
                 self.data[indices] = a
                         
     def send(self, msg):
-        """ SD_bounds_updated messages are sent here """
+        """ SD_reflow_start messages are sent here """
         # print 'Data object got message {0}'.format(msg)
         if self.target is not None:
             # print 'Sending from Data.'
